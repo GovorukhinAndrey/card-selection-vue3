4f12f7fb5d236a8e1b2ee29adc28d7e57a651c1d
Andrei Govorukhin <andr.govorukhin@gmail.com>
2024-11-22 05:33:31 +0000

    feat: UiButton component

---
+++ src/components/ui/UiButton.vue
@@ -0,0 +1,33 @@
+<script lang="ts" setup>
+
+const emits = defineEmits(['click'])
+
+const handleClick = () => {
+  emits('click')
+}
+
+</script>
+
+<template>
+  <button @click="handleClick" v-bind="$attrs">
+    <slot>Submit</slot>
+  </button>
+</template>
+
+<style lang="scss" scoped>
+@use "sass:color";
+
+button {
+  cursor: pointer;
+  border: none;
+  transition: background-color v.$A;
+  border-radius: 10px;
+  background-color: v.$primary;
+  color: v.$white;
+  padding: 10px;
+
+  &:hover {
+    background-color: color.adjust(v.$primary, $lightness: 10%);
+  }
+}
+</style>
